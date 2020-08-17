1a606755e39fe9a69054b8b3ab1da8a297093790
walter <wwan999@aucklanduni.ac.nz>
2020-08-17 12:59:21 +1200

    tile bg color

--- javascript/gameLogic.js
+++ javascript/gameLogic.js
@@ -8,6 +8,7 @@ Game.prototype.execute = function () {
     this.addRandomTile();
     this.addRandomTile();
     this.updateScreen();
+    setBackground(this.cells);
 }
 
 Game.prototype.init = function () {
@@ -80,15 +81,65 @@ function movement(keyCode) {
     }
 
     this.updateScreen();
+    setBackground(this.cells);
 }
 
-function gameOver (cells) {
+function setBackground(cells) {
+    let grid_cells = document.querySelectorAll(".grid_cell");
+    for(let i = 0; i < grid_cells.length; i++){
+        grid_cells[i].className = "grid_cell";
+    }
+    console.log(grid_cells);
+    for (let i = 0; i < cells.length; i++) {
+        switch (cells[i]) {
+            case 2:
+                grid_cells[i].classList.add("one");
+                break;
+            case 4:
+                grid_cells[i].classList.add("two");
+                break;
+            case 8:
+                grid_cells[i].classList.add("three");
+                break;
+            case 16:
+                grid_cells[i].classList.add("four");
+                break;
+            case 32:
+                grid_cells[i].classList.add("five");
+                break;
+            case 64:
+                grid_cells[i].classList.add("six");
+                break;
+            case 128:
+                grid_cells[i].classList.add("seven");
+                break;
+            case 256:
+                grid_cells[i].classList.add("eight");
+                break;
+            case 512:
+                grid_cells[i].classList.add("night");
+                break;
+            case 1024:
+                grid_cells[i].classList.add("ten");
+                break;
+            case 2048:
+            case 4096:
+                grid_cells[i].classList.add("eleven");
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+function gameOver(cells) {
     let matrix = [];
     let index = 0;
     let isGameOver = true;
 
     //if there is empty cells game continue
-    for(let i = 0; i < cells.length; i++){
+    for (let i = 0; i < cells.length; i++) {
         if (cells[i] === null) {
             isGameOver = false;
             return isGameOver;
@@ -97,16 +148,16 @@ function gameOver (cells) {
 
 
     //convent into 2-d matrix
-    for (let i = 0; i < 4; i++){
+    for (let i = 0; i < 4; i++) {
         let row = matrix[i] = [];
-        for(let j = 0; j < 4; j++){
+        for (let j = 0; j < 4; j++) {
             row.push(cells[index++]);
         }
     }
 
     //check horizontal and vertical if there are two tile is same value
-    for (let i = 0; i < 4; i++){
-        for(let j = 0; j < 4; j++){
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
             if (j < 3 && matrix[i][j] === matrix[i][j + 1]) {
                 isGameOver = false;
                 return isGameOver;
@@ -118,12 +169,12 @@ function gameOver (cells) {
         }
     }
 
-    return isGameOver; 
+    return isGameOver;
 }
 
-function getPreviewCells (cells) {
+function getPreviewCells(cells) {
     let previewCells = [];
-    for(let i = 0; i < cells.length; i++){
+    for (let i = 0; i < cells.length; i++) {
         previewCells.push(cells[i]);
     }
     return previewCells;
@@ -131,29 +182,29 @@ function getPreviewCells (cells) {
 
 function stateChange(preview, newCells) {
     let flag = false;
-    for(let i = 0; i < preview.length; i++){
+    for (let i = 0; i < preview.length; i++) {
         if (preview[i] !== newCells[i]) {
             flag = true;
             break;
         }
-    } 
+    }
     return flag;
 }
 
 
-function moveUp (cells) {
+function moveUp(cells) {
     let merge = [], matrix = [];
     let index = 0;
-    for (let i = 0; i < 4; i++){
+    for (let i = 0; i < 4; i++) {
         let row = merge[i] = [];
         let row1 = matrix[i] = [];
-        for(let j = 0; j < 4; j++){
+        for (let j = 0; j < 4; j++) {
             row.push(true);
             row1.push(cells[index++]);
         }
     }
-    for(let i = 1; i < 4; i++){
-        for (let j = 0; j < 4; j++){
+    for (let i = 1; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
             if (matrix[i][j] == null) continue;
             let k = i - 1;
             while (k > 0) {
@@ -196,44 +247,44 @@ function moveDown(cells) {
     return newCells;
 }
 
-function moveLeft (cells) {
+function moveLeft(cells) {
     let left = turnRight(cells);
     let afterMove = moveUp(left);
     let newCells = turnLeft(afterMove);
     return newCells;
 }
 
-function moveRight (cells) {
+function moveRight(cells) {
     let right = turnLeft(cells);
     let afterMove = moveUp(right);
     let newCells = turnRight(afterMove);
     return newCells;
 }
 
-function upsideDown (cells) {
+function upsideDown(cells) {
     let newCells = [];
-    for(let i = 12; i >= 0; i -= 4){
-        for(let j = 0; j < 4; j++){
+    for (let i = 12; i >= 0; i -= 4) {
+        for (let j = 0; j < 4; j++) {
             newCells.push(cells[i + j]);
         }
     }
     return newCells;
 }
 
-function turnLeft (cells) {
+function turnLeft(cells) {
     let newCells = [];
-    for(let i = 3; i >= 0; i--){
-        for (let j = 0; j <= 12; j += 4){
+    for (let i = 3; i >= 0; i--) {
+        for (let j = 0; j <= 12; j += 4) {
             newCells.push(cells[i + j]);
         }
     }
     return newCells;
 }
 
-function turnRight (cells) {
+function turnRight(cells) {
     let newCells = [];
-    for(let i = 12; i < 16; i++){
-        for (let j = 0; j <= 12; j += 4){
+    for (let i = 12; i < 16; i++) {
+        for (let j = 0; j <= 12; j += 4) {
             newCells.push(cells[i - j]);
         }
     }
--- javascript/run.js
+++ javascript/run.js
@@ -5,6 +5,10 @@ window.addEventListener("load", function () {
     window.addEventListener("keydown", function (event) { movement.call(game, event.keyCode) });
 
 
-    let restartButton = document.querySelector(".restart_button");
-    restartButton.addEventListener("click",function(){game.init()} );
+    
+    restartButton();
+    function restartButton() {
+        let restartButton = document.querySelector(".restart_button");
+        restartButton.addEventListener("click", function () { game.init() });
+    }
 })
